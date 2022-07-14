8143013e1cd31d810e48615699fe3acd493151ea
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-07-14 18:06:40 -0300

    social signin button

---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Button, ImageContainer, Title } from "./styles";
+import { SignInSocialButtonProps } from "./types";
+
+const SignInSocialButton: React.FC<SignInSocialButtonProps> = ({
+  title,
+  svg: Svg,
+  ...props
+}) => {
+  return (
+    <Button>
+      <ImageContainer>
+        <Svg />
+      </ImageContainer>
+      <Title>{title}</Title>
+    </Button>
+  );
+};
+
+export default SignInSocialButton;
