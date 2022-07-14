1f79ef96cc84972638e0bc102bd104bbc871c763
Luiz Claudio <lcluizclaudio96@gmail.com>
2022-07-14 18:06:58 -0300

    sigin page

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Text } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import GoogleSvg from "../../assets/google.svg";
+import AppleSvg from "../../assets/apple.svg";
+import LogoSVG from "../../assets/logo.svg";
+import SignInSocialButton from "../../components/SignInSocialButton";
+
+import {
+  Container,
+  Header,
+  Title,
+  TitleWrapper,
+  SignInTitle,
+  Footer,
+  FooterWrapper,
+} from "./styles";
+import { SignInProps } from "./types";
+
+const SignIn: React.FC<SignInProps> = (props) => {
+  return (
+    <Container>
+      <Header>
+        <TitleWrapper>
+          <LogoSVG width={RFValue(120)} height={RFValue(68)} />
+          <Title>
+            Controle suas{"\n"}finanças de forma{"\n"}muito simples
+          </Title>
+        </TitleWrapper>
+        <SignInTitle>Faça seu login com{"\n"}uma das contas abaixo</SignInTitle>
+      </Header>
+      <Footer>
+        <FooterWrapper>
+          <SignInSocialButton title="Entrar com Google" svg={GoogleSvg} />
+          <SignInSocialButton title="Entrar com Apple" svg={AppleSvg} />
+        </FooterWrapper>
+      </Footer>
+    </Container>
+  );
+};
+
+export default SignIn;
